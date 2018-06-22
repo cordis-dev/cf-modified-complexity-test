92813aa7b56dfdb204e518f528990e6b807a5204
Darius <darius@adroiti.com>
2018-06-22 12:30:34 +0300

    update t.groovy

--- t.groovy
+++ t.groovy
@@ -1,5 +1,5 @@
 /**
- * A simple example on how to use switch statement in Groovy.
+ * https://github.com/CodeNarc/CodeNarc/blob/master/src/main/groovy/org/codenarc/rule/size/CyclomaticComplexityRule.groovy
  */
 class SwitchExample {
    static main(args) {
