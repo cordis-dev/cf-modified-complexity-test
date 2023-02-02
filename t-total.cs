71a8dae193b9200b2944a1be3f1f3cac7bac1dcd
cordis-dev <darius@adroiti.com>
2023-02-02 11:51:05 +0200

    Update t-total.cs

--- t-total.cs
+++ t-total.cs
@@ -317,5 +317,34 @@ public class total
 		}
 
 		return text;
-	}		
+	}
+	
+	public string T12(string text)
+	{
+		switch (text)
+		{
+			case "1":
+				return text;
+			case "2":
+				return text;
+			case "3":
+				return text;
+			case "4":
+				return text;
+			case "5":
+				return text;
+			case "6":
+				return text;
+			case "7":
+				return text;
+			case "8":
+				return text;
+			case "9":
+				return text;
+			case "10":
+				return text;			
+		}
+
+		return text;
+	}	
 }
