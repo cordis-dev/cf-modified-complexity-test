4618a02483b423b89be484e75556008acd92b6e9
cordis-dev <darius@adroiti.com>
2024-07-10 12:59:13 +0300

    Create t.java

---
+++ simple/t.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        int num = 3;
+    }
+
+    public static void main2(String[] args) {
+        int num = 4;
+    }
+
+    public static void main3(String[] args) {
+        int num = 5;
+    }
+}
