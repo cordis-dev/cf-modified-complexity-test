2cb8b192d2c0f6f0da5c638e7a8be51a338da2bf
cordis-dev <darius@adroiti.com>
2024-07-10 12:07:33 +0300

    Create t.kt

---
+++ t.kt
@@ -0,0 +1,26 @@
+class SwitchExample {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val num = 3
+            when (num) {
+                1 -> println("1")
+                2 -> println("2")
+                3 -> println("3")
+                4 -> println("4")
+                5 -> println("5")
+                6 -> println("6")
+                7 -> println("7")
+                8 -> println("8")
+                9 -> println("9")
+                10 -> println("10")
+                11 -> println("11")
+                12 -> println("12")
+                13 -> println("13")
+                14 -> println("14")
+                15 -> println("15")
+                else -> println("Something else")
+            }
+        }
+    }
+}
