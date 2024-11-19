3cbe385e543a8530a71f781f12ce159852e3dbca
cordis-dev <darius@adroiti.com>
2024-11-20 01:25:43 +0200

    Create t.py

---
+++ t.py
@@ -0,0 +1,27 @@
+def method_name():
+    car = "Patriot"
+
+    if car == "Focus":
+        manufacturer = "Ford"
+    elif car == "Navigator":
+        manufacturer = "Lincoln"
+    elif car == "Camry":
+        manufacturer = "Toyota"
+    elif car == "Civic":
+        manufacturer = "Honda"
+    elif car == "Patriot":
+        manufacturer = "Jeep"
+    elif car == "Jetta":
+        manufacturer = "VW"
+    elif car == "Ceyene":
+        manufacturer = "Porsche"
+    elif car == "Outback":
+        manufacturer = "Subaru"
+    elif car == "520i":
+        manufacturer = "BMW"
+    elif car == "Tundra":
+        manufacturer = "Nissan"
+    else:
+        manufacturer = "Unknown"
+
+    return manufacturer
