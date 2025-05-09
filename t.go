370018a6c30dea830100b40d25b8d19376c95bd0
cordis-dev <darius@adroiti.com>
2025-05-09 12:53:15 +0300

    Update t.go

--- t.go
+++ t.go
@@ -1,10 +1,41 @@
-// https://github.com/fzipp/gocyclo/blob/master/gocyclo.go
 package main
 
 import "fmt"
 import "time"
 
 func main() {
+
+    if i == 1 {
+        fmt.Println("1")
+    } else if i == 2 {
+        fmt.Println("2")
+    } else if i == 3 {
+        fmt.Println("3")
+    } else if i == 4 {
+        fmt.Println("4")
+    } else if i == 5 {
+        fmt.Println("5")
+    } else if i == 6 {
+        fmt.Println("6")
+    } else if i == 7 {
+        fmt.Println("7")
+    } else if i == 8 {
+        fmt.Println("8")
+    } else if i == 9 {
+        fmt.Println("9")
+    } else if i == 10 {
+        fmt.Println("10")
+    } else if i == 11 {
+        fmt.Println("11")
+    } else if i == 12 {
+        fmt.Println("12")
+    } else if i == 13 {
+        fmt.Println("13")
+    } else if i == 14 {
+        fmt.Println("14")
+    } else if i == 15 {
+        fmt.Println("15")
+    }
 
     i := 2
     fmt.Print("Write ", i, " as ")
