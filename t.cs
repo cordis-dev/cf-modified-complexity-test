5f78fc108e6eae2fe04c2f0b81fad288d3dd0ff4
cordis-dev <darius@adroiti.com>
2025-05-09 11:48:51 +0300

    Update t.cs

--- t.cs
+++ t.cs
@@ -1,6 +1,73 @@
 public class t
 {
-	public string Test(string text)
+	public string IfElseTest(string text)
+	{
+		if (text == "1")
+		{
+			return text;
+		}
+		else if (text == "2")
+		{
+			return text;
+		}
+		else if (text == "3")
+		{
+			return text;
+		}
+		else if (text == "4")
+		{
+			return text;
+		}
+		else if (text == "5")
+		{
+			return text;
+		}
+		else if (text == "6")
+		{
+			return text;
+		}
+		else if (text == "7")
+		{
+			return text;
+		}
+		else if (text == "8")
+		{
+			return text;
+		}
+		else if (text == "9")
+		{
+			return text;
+		}
+		else if (text == "10")
+		{
+			return text;
+		}
+		else if (text == "11")
+		{
+			return text;
+		}
+		else if (text == "12")
+		{
+			return text;
+		}
+		else if (text == "13")
+		{
+			return text;
+		}
+		else if (text == "14")
+		{
+			return text;
+		}
+		else if (text == "15")
+		{
+			return text;
+		}
+
+		// default case
+		return text;
+	}
+
+	public string SwitchTest(string text)
 	{
 		switch (text)
 		{
