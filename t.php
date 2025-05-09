77879cd7e0babb3239258a8137d825e55c970f59
cordis-dev <darius@adroiti.com>
2025-05-09 16:36:44 +0300

    Update t.php

--- t.php
+++ t.php
@@ -1,6 +1,42 @@
 <?php
 // https://github.com/squizlabs/PHP_CodeSniffer/blob/master/src/Standards/Generic/Sniffs/Metrics/CyclomaticComplexitySniff.php
-public function test()
+
+public function ifElseifTest()
+{
+    if ($i == "1") {
+        echo "1";
+    } elseif ($i == "2") {
+        echo "2";
+    } elseif ($i == "3") {
+        echo "3";
+    } elseif ($i == "4") {
+        echo "4";
+    } elseif ($i == "5") {
+        echo "5";
+    } elseif ($i == "6") {
+        echo "6";
+    } elseif ($i == "7") {
+        echo "7";
+    } elseif ($i == "8") {
+        echo "8";
+    } elseif ($i == "9") {
+        echo "9";
+    } elseif ($i == "10") {
+        echo "10";
+    } elseif ($i == "11") {
+        echo "11";
+    } elseif ($i == "12") {
+        echo "12";
+    } elseif ($i == "13") {
+        echo "13";
+    } elseif ($i == "14") {
+        echo "14";
+    } elseif ($i == "15") {
+        echo "15";
+    }
+}
+
+public function swtichTest()
 {
 	switch ($i) {
 		case "1":
