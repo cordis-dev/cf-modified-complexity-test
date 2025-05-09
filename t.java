c48675f1755cd42a53232fdac313209797422d02
cordis-dev <darius@adroiti.com>
2025-05-09 16:07:16 +0300

    Update t.java

--- t.java
+++ t.java
@@ -1,5 +1,44 @@
-public class SwitchExample {
-    public static void main(String[] args) {
+public class Complexity {
+
+    public static void ifElseTest(String[] args) {
+        int num = 3;
+    
+        if (num == 1) {
+            System.out.println("1");
+        } else if (num == 2) {
+            System.out.println("2");
+        } else if (num == 3) {
+            System.out.println("3");
+        } else if (num == 4) {
+            System.out.println("4");
+        } else if (num == 5) {
+            System.out.println("5");
+        } else if (num == 6) {
+            System.out.println("6");
+        } else if (num == 7) {
+            System.out.println("7");
+        } else if (num == 8) {
+            System.out.println("8");
+        } else if (num == 9) {
+            System.out.println("9");
+        } else if (num == 10) {
+            System.out.println("10");
+        } else if (num == 11) {
+            System.out.println("11");
+        } else if (num == 12) {
+            System.out.println("12");
+        } else if (num == 13) {
+            System.out.println("13");
+        } else if (num == 14) {
+            System.out.println("14");
+        } else if (num == 15) {
+            System.out.println("15");
+        } else {
+            System.out.println("Something else");
+        }
+    }
+
+    public static void switchTest(String[] args) {
         int num = 3;
         switch (num) {
             case 1:
