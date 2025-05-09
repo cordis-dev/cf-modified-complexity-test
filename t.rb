b81e6493bee8ce2a1bca374de62533cdfc4dbfca
cordis-dev <darius@adroiti.com>
2025-05-09 15:03:08 +0300

    Update t.rb

--- t.rb
+++ t.rb
@@ -1,6 +1,37 @@
 # https://github.com/rubocop-hq/rubocop/blob/master/lib/rubocop/cop/metrics/cyclomatic_complexity.rb
 # https://github.com/rubocop-hq/rubocop/blob/master/lib/rubocop/cop/metrics/perceived_complexity.rb
-def method_name
+def if_elseif_test
+  car = "Patriot"
+
+  manufacturer =
+    if car == "Focus"
+      "Ford"
+    elsif car == "Navigator"
+      "Lincoln"
+    elsif car == "Camry"
+      "Toyota"
+    elsif car == "Civic"
+      "Honda"
+    elsif car == "Patriot"
+      "Jeep"
+    elsif car == "Jetta"
+      "VW"
+    elsif car == "Ceyene"
+      "Porsche"
+    elsif car == "Outback"
+      "Subaru"
+    elsif car == "520i"
+      "BMW"
+    elsif car == "Tundra"
+      "Nissan"
+    else
+      "Unknown"
+    end
+
+  manufacturer
+end
+
+def case_when_test
 	car = "Patriot"
 
 	manufacturer = case car
