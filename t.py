8eb178829f4caf663e3032ade9d56e7800fbf640
cordis-dev <darius@adroiti.com>
2025-05-12 17:06:49 +0300

    Update t.py

--- t.py
+++ t.py
@@ -1,4 +1,43 @@
-def method_name():
+def match_case_test():
+    car = "Patriot"
+    
+    match car:
+        case "Focus":
+            manufacturer = "Ford"
+        case "Navigator":
+            manufacturer = "Lincoln"
+        case "Camry":
+            manufacturer = "Toyota"
+        case "Civic":
+            manufacturer = "Honda"
+        case "Patriot":
+            manufacturer = "Jeep"
+        case "Jetta":
+            manufacturer = "VW"
+        case "Ceyene":
+            manufacturer = "Porsche"
+        case "Outback":
+            manufacturer = "Subaru"
+        case "520i":
+            manufacturer = "BMW"
+        case "Tundra":
+            manufacturer = "Nissan"
+        case "Tundra2":
+            manufacturer = "Nissan2"
+        case "Tundra3":
+            manufacturer = "Nissan3"
+        case "Tundra4":
+            manufacturer = "Nissan4"
+        case "Tundra5":
+            manufacturer = "Nissan5"
+        case "Tundra6":
+            manufacturer = "Nissan6"
+        case _:
+            manufacturer = "Unknown"
+    
+    return manufacturer
+
+def if_elseif_test():
     car = "Patriot"
 
     if car == "Focus":
