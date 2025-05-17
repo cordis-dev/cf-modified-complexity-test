f6548214ca96033e1d2264a335843ca4b9ca07f8
cordis-dev <darius@adroiti.com>
2025-05-17 10:02:11 +0300

    Create t2.cs

---
+++ t2.cs
@@ -0,0 +1,116 @@
+public class t2
+{
+	public string IfElseTest(string text)
+	{
+		if (text == "1")
+		{
+			return text;
+		}
+		else if (text == "2")
+		{
+			return text;
+		}
+		else if (text == "3")
+		{
+			return text;
+		}
+		else if (text == "4")
+		{
+			return text;
+		}
+		else if (text == "5")
+		{
+			return text;
+		}
+		else if (text == "6")
+		{
+			return text;
+		}
+		else if (text == "7")
+		{
+			return text;
+		}
+		else if (text == "8")
+		{
+			return text;
+		}
+		else if (text == "9")
+		{
+			return text;
+		}
+		else if (text == "10")
+		{
+			return text;
+		}
+		else if (text == "11")
+		{
+			return text;
+		}
+		else if (text == "12")
+		{
+			return text;
+		}
+		else if (text == "13")
+		{
+			return text;
+		}
+		else if (text == "14")
+		{
+			return text;
+		}
+		else if (text == "15")
+		{
+			return text;
+		}
+		else if (text == "16")
+		{
+			return text;
+		}		
+
+		// default case
+		return text;
+	}
+
+	public string SwitchTest(string text)
+	{
+		switch (text)
+		{
+			case "1":
+				return text;
+			case "2":
+				return text;
+			case "3":
+				return text;
+			case "4":
+				return text;
+			case "5":
+				return text;
+			case "6":
+				return text;
+			case "7":
+				return text;
+			case "8":
+				return text;
+			case "9":
+				return text;
+			case "10":
+				return text;
+			case "11":
+				return text;
+			case "12":
+				return text;
+			case "13":
+				return text;
+			case "14":
+				return text;
+			case "15":
+				return text;
+			case "16":
+				return text;
+			case "17":
+				return text;
+		}
+
+		return text;
+	}
+}
