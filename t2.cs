88b6e23794e8a458e6d73b39b20a71e44244a0c8
cordis-dev <darius@adroiti.com>
2025-05-17 10:03:26 +0300

    Update t2.cs

--- t2.cs
+++ t2.cs
@@ -5,67 +5,7 @@ public class t2
 		if (text == "1")
 		{
 			return text;
-		}
-		else if (text == "2")
-		{
-			return text;
-		}
-		else if (text == "3")
-		{
-			return text;
-		}
-		else if (text == "4")
-		{
-			return text;
-		}
-		else if (text == "5")
-		{
-			return text;
-		}
-		else if (text == "6")
-		{
-			return text;
-		}
-		else if (text == "7")
-		{
-			return text;
-		}
-		else if (text == "8")
-		{
-			return text;
-		}
-		else if (text == "9")
-		{
-			return text;
-		}
-		else if (text == "10")
-		{
-			return text;
-		}
-		else if (text == "11")
-		{
-			return text;
-		}
-		else if (text == "12")
-		{
-			return text;
-		}
-		else if (text == "13")
-		{
-			return text;
-		}
-		else if (text == "14")
-		{
-			return text;
-		}
-		else if (text == "15")
-		{
-			return text;
-		}
-		else if (text == "16")
-		{
-			return text;
-		}		
+		}	
 
 		// default case
 		return text;
