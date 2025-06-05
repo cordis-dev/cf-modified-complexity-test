4a95227295f3c0c1bfd369f1584d8acd7ffd7086
cordis-dev <darius@adroiti.com>
2025-06-05 13:32:44 +0300

    Create t.swift

---
+++ t.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+class T {
+    
+    static func ifElseTest() {
+        let num = 3
+        
+        if num == 1 {
+            print("1")
+        } else if num == 2 {
+            print("2")
+        } else if num == 3 {
+            print("3")
+        } else if num == 4 {
+            print("4")
+        } else if num == 5 {
+            print("5")
+        } else if num == 6 {
+            print("6")
+        } else if num == 7 {
+            print("7")
+        } else if num == 8 {
+            print("8")
+        } else if num == 9 {
+            print("9")
+        } else if num == 10 {
+            print("10")
+        } else if num == 11 {
+            print("11")
+        } else if num == 12 {
+            print("12")
+        } else if num == 13 {
+            print("13")
+        } else if num == 14 {
+            print("14")
+        } else if num == 15 {
+            print("15")
+        } else {
+            print("Something else")
+        }
+    }
+    
+    static func switchTest() {
+        let num = 3
+        
+        switch num {
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        case 3:
+            print("3")
+        case 4:
+            print("4")
+        case 5:
+            print("5")
+        case 6:
+            print("6")
+        case 7:
+            print("7")
+        case 8:
+            print("8")
+        case 9:
+            print("9")
+        case 10:
+            print("10")
+        case 11:
+            print("11")
+        case 12:
+            print("12")
+        case 13:
+            print("13")
+        case 14:
+            print("14")
+        case 15:
+            print("15")
+        default:
+            print("Something else")
+        }
+    }
+}
